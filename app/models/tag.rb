5bebeb0c8754d52ad910c395b8b565c602dd7d2e
Jake Boxer <jacob.boxer@gmail.com>
2010-02-03 16:50:58 -0500

    Added a short comment to explain Tags, and protected Tags with attr_accessible

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,10 @@
+# Used to mark an Address with different meanings of items of interest.
 class Tag < ActiveRecord::Base
   has_many :taggings, :dependent => :destroy
   has_many :addresses, :through => :taggings
   
   # A tag name must be at least one character long and contain no commas
   validates_format_of :name, :with => /\A[^,]+\Z/
+  
+  attr_accessible :name
 end
