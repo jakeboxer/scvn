c46d45e959447815af4e15682076d57a6f48e44e
Jake Boxer <jacob.boxer@gmail.com>
2010-02-03 00:55:59 -0500

    Added unit test for finding addresses by key

--- test/unit/address_test.rb
+++ test/unit/address_test.rb
@@ -1,12 +1,19 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  test "convert between numerical IDs and shortened keys" do
+  test 'convert between numerical IDs and shortened keys' do
     scvngr = addresses(:scvngr)
     jboxer = addresses(:jboxer)
     assert_equal scvngr.key, Address.id_to_key(scvngr.id)
     assert_equal scvngr.id,  Address.key_to_id(scvngr.key)
     assert_equal jboxer.key, Address.id_to_key(jboxer.id)
     assert_equal jboxer.id,  Address.key_to_id(jboxer.key)
+  end
+  
+  test 'find by key' do
+    scvngr = addresses(:scvngr)
+    jboxer = addresses(:jboxer)
+    assert_equal scvngr, Address.find_by_key(scvngr.key)
+    assert_equal jboxer, Address.find_by_key(jboxer.key)
   end
 end
