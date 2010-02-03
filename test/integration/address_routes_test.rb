79ab9dbc6caf7c303486ddd95eee152f2bbf31d9
Jake Boxer <jacob.boxer@gmail.com>
2010-02-03 13:35:22 -0500

    Added basic integration tests

---
+++ test/integration/address_routes_test.rb
@@ -0,0 +1,47 @@
+require 'test_helper'
+
+class AddressRoutesTest < ActionController::IntegrationTest
+  fixtures :all
+  
+  test "GET /" do
+    # Root should go to the new addresses page
+    get '/'
+    assert_response :success
+    assert_template 'addresses/new'
+  end
+  
+  test "GET /:shortened" do
+    get "/#{addresses(:scvngr).shortened}"
+    assert_redirected_to addresses(:scvngr).url
+  end
+  
+  test "GET /unshortened/addresses/:shortened" do
+    get "/unshortened/addresses/#{addresses(:scvngr).shortened}"
+    assert_response :success
+    assert_template 'addresses/show'
+  end
+  
+  test "GET /unshortened/addresses/:shortened/edit" do
+    get "/unshortened/addresses/#{addresses(:scvngr).shortened}/edit"
+    assert_response :success
+    assert_template 'addresses/edit'
+  end
+  
+  test "PUT /unshortened/addresses/:shortened" do
+    put "/unshortened/addresses/#{addresses(:scvngr).shortened}",
+      :address => { :url => 'http://www.google.com/' }
+    assert_redirected_to addresses(:scvngr)
+    addresses(:scvngr).reload
+    assert_equal 'http://www.google.com/', addresses(:scvngr).url
+  end
+  
+  test "POST /unshortened/addresses" do
+    cond = { :url => 'http://www.realultimatepower.net/index4.htm' }
+    assert_difference 'Address.count(:conditions => cond)', 1 do
+      post "/unshortened/addresses",
+        :address => { :url => 'http://www.realultimatepower.net/index4.htm' }
+    end
+    
+    assert_redirected_to assigns(:address)
+  end
+end
