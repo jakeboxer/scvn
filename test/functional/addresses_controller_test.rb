fc5ea797a68e70a53bf7998ee667fe10d4153ad8
Jake Boxer <jacob.boxer@gmail.com>
2010-02-03 02:07:10 -0500

    Unified `shortened` and `key` in functional tests

--- test/functional/addresses_controller_test.rb
+++ test/functional/addresses_controller_test.rb
@@ -15,48 +15,30 @@ class AddressesControllerTest < ActionController::TestCase
   end
 
   test "should show address" do
-    get :show, :key => addresses(:scvngr).key
-    assert_response :success
-    
-    get :show, :id  => addresses(:scvngr).to_param
+    get :show, :id  => addresses(:scvngr).shortened
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :key => addresses(:scvngr).key
-    assert_response :success
-    
-    get :edit, :id  => addresses(:scvngr).to_param
+    get :edit, :id  => addresses(:scvngr).shortened
     assert_response :success
   end
 
   test "should update address" do
-    put :update, :key => addresses(:scvngr).key, :address => { }
-    assert_redirected_to address_path(assigns(:address))
-    
-    put :update, :id  => addresses(:scvngr).to_param, :address => { }
+    put :update, :id  => addresses(:scvngr).shortened, :address => { }
     assert_redirected_to address_path(assigns(:address))
   end
 
   test "should destroy address" do
     assert_difference('Address.count', -1) do
-      delete :destroy, :key => addresses(:scvngr).key
-    end
-
-    assert_redirected_to addresses_path
-    
-    assert_difference('Address.count', -1) do
-      delete :destroy, :id => addresses(:jboxer).to_param
+      delete :destroy, :id => addresses(:scvngr).shortened
     end
 
     assert_redirected_to addresses_path
   end
   
   test "should redirect to address's url" do
-    get :goto, :key => addresses(:scvngr).key
-    assert_redirected_to addresses(:scvngr).url
-    
-    get :goto, :id => addresses(:scvngr).to_param
+    get :goto, :id => addresses(:scvngr).shortened
     assert_redirected_to addresses(:scvngr).url
   end
 end
