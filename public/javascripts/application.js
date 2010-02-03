83265f2324015cd792979fa15bf753a5c35119cc
Jake Boxer <jacob.boxer@gmail.com>
2010-02-03 14:21:41 -0500

    Added text box hinting

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,28 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function setupHints (hasHintClass, showHintClass) {
+	$('input.' + hasHintClass).each(function () {
+		// Grab the text box and its title
+		var box      = $(this);
+		var boxTitle = box.attr('title');
+		
+		if(boxTitle != ''){
+			// If the text box has a title, set up the blur and focus functions
+			box.blur(function () {
+				if(box.val() == ''){
+					box.val(boxTitle);
+					box.addClass(showHintClass);
+				}
+			});
+			
+			box.focus(function () {
+				if(box.hasClass(showHintClass) && (box.val() === boxTitle)){
+					box.val('');
+					box.removeClass(showHintClass);
+				}
+			});
+		}
+	});
+}
+
+$(document).ready(function () {
+	setupHints('has-hint', 'show-hint');
+})
