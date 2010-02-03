52d8b2503c78edeb411d516a5be60008c0d709da
Jake Boxer <jacob.boxer@gmail.com>
2010-02-03 16:58:04 -0500

    Added short comment explaining Tagging, and blocked all attributes from being mass-assigned

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,4 +1,8 @@
+# Represents a single Address being tagged with a single Tag
 class Tagging < ActiveRecord::Base
   belongs_to :address
   belongs_to :tag
+  
+  # Block any attributes from being mass-assigned
+  attr_accessible 
 end
