94574ec2d1971afa5ec9ad862880a3732a9361a6
Jake Boxer <jacob.boxer@gmail.com>
2010-02-03 16:37:16 -0500

    Added validation to tags, so all tags are at least one character long and contain no commas

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ActiveRecord::Base
   has_many :taggings, :dependent => :destroy
   has_many :addresses, :through => :taggings
+  
+  # A tag name must be at least one character long and contain no commas
+  validates_format_of :name, :with => /\A[^,]+\Z/
 end
