30cfc631a1c574a55b5b40a7d928419e802eab2e
Jake Boxer <jacob.boxer@gmail.com>
2010-02-03 14:34:31 -0500

    Made hints blur on page load, fixed hints being submitted if not overwritten, and factored out some duplication with showing and hiding hints

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,26 +1,47 @@
+// Sets up hint text on all input boxes with titles and the specified class.
 function setupHints (hasHintClass, showHintClass) {
 	$('input.' + hasHintClass).each(function () {
 		// Grab the text box and its title
 		var box      = $(this);
 		var boxTitle = box.attr('title');
 		
+		// If the text box has a title...
 		if(boxTitle != ''){
-			// If the text box has a title, set up the blur and focus functions
+			// Set up the blur function
 			box.blur(function () {
-				if(box.val() == ''){
-					box.val(boxTitle);
-					box.addClass(showHintClass);
-				}
+				showHint(box, showHintClass);
 			});
 			
+			// Set up the focus function
 			box.focus(function () {
-				if(box.hasClass(showHintClass) && (box.val() === boxTitle)){
-					box.val('');
-					box.removeClass(showHintClass);
-				}
+				clearHint(box, showHintClass);
+			});
+			
+			// Start the box off as blurred
+			box.blur();
+			
+			// When the form is submitted, clear the hint
+			$(this.form).submit(function () {
+				clearHint(box, showHintClass);
 			});
 		}
 	});
+}
+
+// Show a text box's hint
+function showHint (box, showHintClass) {
+	if(box.val() == ''){
+		box.val(box.attr('title'));
+		box.addClass(showHintClass);
+	}
+}
+
+// Clear a hint from the text box
+function clearHint (box, showHintClass) {
+	if(box.hasClass(showHintClass) && (box.val() === box.attr('title'))){
+		box.val('');
+		box.removeClass(showHintClass);
+	}
 }
 
 $(document).ready(function () {
