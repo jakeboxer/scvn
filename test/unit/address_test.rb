ba9889c99e9a174e9464d79a14a80c637947524e
Jake Boxer <jacob.boxer@gmail.com>
2010-02-03 00:13:48 -0500

    Added test for URL-shortening

--- test/unit/address_test.rb
+++ test/unit/address_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "convert between numerical IDs and shortened keys" do
+    assert_equal addresses(:scvngr).key, key_to_id(addresses(:scvngr).id)
+    assert_equal addresses(:scvngr).id,  id_to_key(addresses(:scvngr).key)
+    assert_equal addresses(:jboxer).key, key_to_id(addresses(:jboxer).id)
+    assert_equal addresses(:jboxer).id,  id_to_key(addresses(:jboxer).key)
   end
 end
