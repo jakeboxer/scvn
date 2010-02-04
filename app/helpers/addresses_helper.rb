a928d52fe342daf8bd0759232576aedc126b0a01
Jake Boxer <jacob.boxer@gmail.com>
2010-02-04 00:20:41 -0500

    Added visitor counter and 'Tweet this' link

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -1,2 +1,9 @@
 module AddressesHelper
+  def twitter_url (address)
+    hashtags = address.tags.map {|tag| "##{tag.name}"}
+    tag_txt  = hashtags.join(' ')
+    tweet    = URI.encode("Check out #{goto_shortened_url(address)} #{tag_txt}")
+    txt      = truncate(tweet, :length => 140, :omission => '..')
+    "http://twitter.com/?status=#{txt}"
+  end
 end
